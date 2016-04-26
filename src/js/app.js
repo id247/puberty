1fed102d2c4975733ac367d0a04f265be2639b79
simplecss <simplecss@yandex.ru>
2016-04-26 15:14:07 +0300

    new config for dev

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,66 @@
 'use strict';
 
-import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { emit } from './lib/dispatcher';
+import actions from './lib/actions';
+import { getState, addChangeListener } from './stores/store';
 
-const app = ( () => {
+//pages
+// import Loading from './components/pages/loading';
 
-	function init(){
 
-	}
-	
-	return{
-		init: init
-	}
-})();
+//import 'babel-polyfill';
+
+const app = (settings) => {
+
+	const App = React.createClass({
+		getInitialState() {
+
+			return getState();
+		},
+		componentDidMount() {
+
+			addChangeListener(this._update);
+
+			emit(actions.SET_SETTINGS, {
+				settings: this.props.settings,
+				page: 'greeting'			
+			});
+		},
+		_update() {
+
+			this.setState(getState());
+		},
+		render() {
+
+			console.log('RENDER');
+
+			let page;
+
+			switch(this.state.page){
+				// case 'result': page = <Result 	pers={this.state.pers}
+				// 								server={this.state.settings.server}
+				// 								shares={this.state.shares}
+				// 								/>; 
+				// 	break;
+
+				// default: page = <ErrorPage />;
+			}
+
+			return (
+				<div>
+				</div>
+			)
+		}
+	});
+
+	ReactDOM.render(
+		<App 	settings={settings} 
+		/>, 
+		document.getElementById('app')
+	);
+
+};
 
 export default app;
